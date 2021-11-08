6713a4bc47c8d11312e00e17b810e315b2a65e05
AdsonNascimento <adson.nascimento.112@ufrn.edu.br>
2021-11-08 11:04:13 -0300

    media
    media 1200px

--- main.js
+++ main.js
@@ -39,7 +39,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /*===========scrollreveal=============*/
@@ -77,3 +83,5 @@ window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
 })
+
+/* menu ativo conforme seção ativa na pagina */
